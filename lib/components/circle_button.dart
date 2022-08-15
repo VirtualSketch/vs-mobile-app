83e698ea60c24d1a1221b6c58355611326e96211
JorgeFernandoCR <jokanjii25@gmail.com>
2022-08-14 21:19:00 -0300

    ✨ Insercao do botao

---
+++ lib/components/circle_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CircleButton extends StatelessWidget {
+  const CircleButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            /*Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const MainPage(),
+              ),
+            );*/
+          },
+          child: const Icon(
+            Icons.circle,
+            color: Colors.white,
+          ),
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size(125, 125),
+            shape: const CircleBorder(),
+            primary: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
