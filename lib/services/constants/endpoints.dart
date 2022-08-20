b8e371fce09892e1ddd254811925ea19e83228a9
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-08-19 22:27:17 -0300

    ✨ Create dio client api

---
+++ lib/services/constants/endpoints.dart
@@ -0,0 +1,7 @@
+class Endpoints {
+  Endpoints._();
+
+  static const String createSession = '/create_session';
+  static const String getGraph = '/get_graph';
+  static const String getResolvedAccount = '/get_equation';
+}
