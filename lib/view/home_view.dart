ecde17e486d3da1c5df2f6cbc3c96654cbea725e
JorgeFernandoCR <jokanjii25@gmail.com>
2022-08-20 09:50:09 -0300

    🔥 remove history from mainframe

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../components/history.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
