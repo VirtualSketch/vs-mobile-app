8f0624db4c9d059117ad9e7cad97788c2be91703
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-08-23 19:43:03 -0300

    🔥 Remove useless prop to json serializable

--- lib/model/create_session_model.dart
+++ lib/model/create_session_model.dart
@@ -2,11 +2,12 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'create_session_model.g.dart';
 
-@JsonSerializable(explicitToJson: true)
+@JsonSerializable()
 class CreateSessionModel {
-  @JsonKey(name: 'session_id') final String? sessionId;
+  @JsonKey(name: 'session_id')
+  final String sessionId;
 
-  CreateSessionModel({this.sessionId});
+  CreateSessionModel({required this.sessionId});
 
   factory CreateSessionModel.fromJson(Map<String, dynamic> json) {
     return _$CreateSessionModelFromJson(json);
