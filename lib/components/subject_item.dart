e3aa1a929463a9db418d980d9e52175498441ed8
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-09-06 21:24:52 -0300

    ✨ Mapping subjects list to render subject items

--- lib/components/subject_item.dart
+++ lib/components/subject_item.dart
@@ -1,45 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SubjectItem extends StatelessWidget {
-  const SubjectItem({Key? key, required this.name}) : super(key: key);
+  const SubjectItem(
+      {Key? key,
+      required this.name,
+      required this.icon,
+      this.isDisabled = false})
+      : super(key: key);
 
   final String name;
+  final IconData icon;
+  final bool isDisabled;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: const BorderRadius.all(Radius.circular(30)),
-      onTap: () => print('test'),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(30)),
-        child: Container(
+    return Container(
+        decoration: BoxDecoration(
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black26, offset: Offset(0, 1), blurRadius: 2.0)
+          ],
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Material(
+          borderRadius: BorderRadius.circular(12.0),
           color: const Color(0xffEDF6F9),
-          child: Padding(
-            padding: const EdgeInsets.all(5),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: const Icon(Icons.functions,
-                      size: 40, color: Color(0xFF8075FF)),
-                ),
-                Text(
-                  name,
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.nunito(
-                    textStyle: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 18,
-                      color: Color(0xFF8075FF),
+          child: isDisabled
+              ? Container(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FaIcon(icon, color: const Color(0xFF8075FF)),
+                      const SizedBox(
+                        height: 4.0,
+                      ),
+                      Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.nunito(
+                          textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Color(0xFF8075FF)),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(icon, color: const Color(0xFF8075FF)),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          name,
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.nunito(
+                            textStyle: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Color(0xFF8075FF)),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/components/subject_screen.dart
+++ lib/components/subject_screen.dart
@@ -1,41 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:virtual_sketch_app/components/custom_close_button.dart';
 import 'package:virtual_sketch_app/components/section_template.dart';
 import 'package:virtual_sketch_app/components/subject_item.dart';
 
+typedef Subject = List<Map<String, dynamic>>;
+
 class SubjectScreen extends StatelessWidget {
-  const SubjectScreen({Key? key}) : super(key: key);
+  const SubjectScreen({Key? key, required this.pageViewController})
+      : super(key: key);
+
+  final PageController pageViewController;
+
+  static const Subject subjects = [
+    {
+      'name': 'Math',
+      'icon': FontAwesomeIcons.squareRootVariable,
+      'isDisabled': false
+    },
+    {
+      'name': 'Coming Soon',
+      'icon': FontAwesomeIcons.screwdriverWrench,
+      'isDisabled': true
+    },
+    {
+      'name': 'Coming Soon',
+      'icon': FontAwesomeIcons.screwdriverWrench,
+      'isDisabled': true
+    },
+    {
+      'name': 'Coming Soon',
+      'icon': FontAwesomeIcons.screwdriverWrench,
+      'isDisabled': true
+    },
+    {
+      'name': 'Coming Soon',
+      'icon': FontAwesomeIcons.screwdriverWrench,
+      'isDisabled': true
+    },
+    {
+      'name': 'Coming Soon',
+      'icon': FontAwesomeIcons.screwdriverWrench,
+      'isDisabled': true
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SectionTemplate(
+      title: 'Choose a subject',
+      subTitle: 'to start to learn',
       bgColor: const Color(0xFF7A44EC),
-      closeButtonWidget: CustomCloseButton(onClose: () => {}),
+      closeButtonWidget: CustomCloseButton(onClose: _handleCloseButton),
       child: GridView.count(
         crossAxisCount: 3,
         crossAxisSpacing: 25,
         mainAxisSpacing: 32,
-        // childAspectRatio: 3 / 2,
-        padding: const EdgeInsetsDirectional.all(15),
-        children: const [
-          SubjectItem(name: 'Math'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-          SubjectItem(name: 'Coming Soon'),
-        ],
+        children: subjects
+            .map((subject) => SubjectItem(
+                name: subject['name'],
+                icon: subject['icon'],
+                isDisabled: subject['isDisabled']))
+            .toList(),
       ),
     );
+  }
+
+  void _handleCloseButton() {
+    pageViewController.animateToPage(1,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
   }
 }
